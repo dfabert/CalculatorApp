4b510dfa7fa35e5f544cfcca9a51a44e6cd859f8
Glen D Fabert <glen.fabert@gmail.com>
2020-12-02 21:57:33 -0500

    fixed navbar issues to avoid trouble with deployment

--- client/src/NavTabs.js
+++ client/src/NavTabs.js
@@ -2,22 +2,22 @@ import React from "react";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from "react-router-dom";
 
 
 function NavTabs() {
-  var activeKey = '1';
-  const handleSelect  = (eventKey) => activeKey = '${eventKey}';
 
   return (
 <>
   <Navbar bg="dark" variant="dark" sticky='top'>
-    <Navbar.Brand href="/">OmniCalculator</Navbar.Brand>
-    <Nav className="justify-content-center" variant='pills' onSelect={handleSelect}>
-      <Nav.Link eventKey='1' href="/">Home</Nav.Link>
-      <Nav.Link eventKey='2' href="/Basic">Basic</Nav.Link>
+    <Navbar.Brand as={Link} to="/">OmniCalculator</Navbar.Brand>
+    <Nav className="justify-content-center">
+      <Nav.Link as={Link} to="/">Home</Nav.Link>
+      <Nav.Link as={Link} to="/Basic">Basic</Nav.Link>
       <NavDropdown title="Financial" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1"><a href="/Financial/Savings">Savings</a></NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.1" as={Link} to="/Financial/Savings">Savings</NavDropdown.Item>
         <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.1" as={Link} to="/Financial/Savings">Savings</NavDropdown.Item>
       </NavDropdown>
     </Nav>
   </Navbar>
