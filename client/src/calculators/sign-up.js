428a1b99b402dee6a01aacfa0af891580e182761
sizyra <sisaaczyra@gmail.com>
2020-12-03 18:30:52 -0800

    Added dependencies, config, sign-up.js
    Dependencies added: bcrypt, client-passport, react-passport, if-env, morgan

---
+++ client/src/calculators/sign-up.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+class SignupForm extends React.Component {
+    handleSubmit(e) {
+        e.preventDefault();
+        
+        console.log(`signup form, username: ${this.state.username}`);
+    
+        axios.post('/', {
+            username: this.state.username,
+            password: this.state.password
+        }).then (response => {
+            console.log(response);
+            if (response.data) {
+                console.log('Successful signup!');
+                this.setState({
+                    redirectTo: '/login'
+                });
+            } else {
+                console.log('Signup error.');
+            }
+        }).catch(error => {
+            console.log(`Signup server error: ${error}`)
+        });
+    }
+
+    render() {
+        <form onSubmit={this.handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={this.state.username} onChange={this.handleChange} />
+            </label>
+            <label>
+                Password:
+                <input type="text" value={this.state.password} onchange={this.handleChange} />
+            </label>
+            <input type="submit" value="Submit" />
+        </form>
+    }
+}
