987be3c0540079dffcb6df306dbbcaeec26d192a
sizyra <sisaaczyra@gmail.com>
2020-12-05 09:07:27 -0800

    Back end fixes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,5 +3,10 @@ const postRoutes = require("./posts");
 
 // Post routes
 
+<<<<<<< Updated upstream
+=======
+// Calculation and Answer routes
+router.use('/calculations', postRoutes);
+>>>>>>> Stashed changes
 
 module.exports = router;
