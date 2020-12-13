44955e4b817baaec40b60bc9d6b1d951ce57e38a
Glen D Fabert <glen.fabert@gmail.com>
2020-12-12 19:23:13 -0500

    wired up budget to the navbar

--- client/src/calculators/Budget.js
+++ client/src/calculators/Budget.js
@@ -2,53 +2,53 @@ import React,{useState, useEffect} from "react";
 import './Savings.scss';
 
 function Savings() {
-  const [expenditures, setExpenditures]= useState([]);
-  calculateSavings = () => {
-    var arrayLength = expenditures.length;
-    var totalExpenses = 0;
+  // const [expenditures, setExpenditures]= useState([]);
+  // calculateSavings = () => {
+  //   var arrayLength = expenditures.length;
+  //   var totalExpenses = 0;
   
-    for (var i = 0; i < arrayLength; i++) {
-      totalExpenses =+ parseInt(expenditures[i].slider_amount) + totalExpenses;
-    }
+  //   for (var i = 0; i < arrayLength; i++) {
+  //     totalExpenses =+ parseInt(expenditures[i].slider_amount) + totalExpenses;
+  //   }
   
-    var yearlyIncome = parseInt(incomes[0].amount);
-    var monthlyIncome = yearlyIncome / 12;
+  //   var yearlyIncome = parseInt(incomes[0].amount);
+  //   var monthlyIncome = yearlyIncome / 12;
   
-    var totalSavingsPerMonth = monthlyIncome - totalExpenses;
+  //   var totalSavingsPerMonth = monthlyIncome - totalExpenses;
   
   
-    this.setState({
-      monthly_income: Math.round(monthlyIncome),
-      monthly_expenses: Math.round(totalExpenses),
-      monthly_savings: Math.round(totalSavingsPerMonth),
-    })
-  }
+  //   this.setState({
+  //     monthly_income: Math.round(monthlyIncome),
+  //     monthly_expenses: Math.round(totalExpenses),
+  //     monthly_savings: Math.round(totalSavingsPerMonth),
+  //   })
+  // }
   
-  handleFeedback = (event) => {
-    this.setState({
-      feedback: event.target.id
-    })
-  }
+  // handleFeedback = (event) => {
+  //   this.setState({
+  //     feedback: event.target.id
+  //   })
+  // }
   
-  handleExpenseChange = (event) => {
-    let index = event.target.id;
-    // Strip pound sign
-    var expenditureNumber = event.target.value.replace(/\D/g,''); 
+  // handleExpenseChange = (event) => {
+  //   let index = event.target.id;
+  //   // Strip pound sign
+  //   var expenditureNumber = event.target.value.replace(/\D/g,''); 
   
-    this.setState({
-      expenditures: update(this.state.expenditures, {
-        [index]: {
-          amount: {$set: expenditureNumber}
-        }
-      })
-    },() => {
-      this.calculateSavings();
-    })
-  }
+  //   this.setState({
+  //     expenditures: update(this.state.expenditures, {
+  //       [index]: {
+  //         amount: {$set: expenditureNumber}
+  //       }
+  //     })
+  //   },() => {
+  //     this.calculateSavings();
+  //   })
+  // }
   
   return (
     <div>
-      Savings Calculator
+      Budget Calculator
     </div>
   );
 }
